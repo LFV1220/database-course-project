e49e3f48af8ebd03b21af376f46e3f58a22b2a00
Luis F. Vega <vegaluis1220@gmail.com>
2022-09-27 21:20:04 -0400

    adding the campus map

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,11 +1,13 @@
-
+import campusMap from './assets/usf_campus_map_auto.png';
 
 function Map() {
     return (
         <div>
-            <h1>Test</h1>
+            <img className="campus-map" src={campusMap} alt="USF Campus Map" width="100%" />
         </div>
     );
 }
 
 export default Map;
+
+// NOTE: Change image, used website to enhance image so made it inaccurate
