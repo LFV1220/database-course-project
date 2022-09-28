fbdd9b042ea534749100c86e32bc31bb079026cf
Adrian <grillo.adrian13@gmail.com>
2022-09-28 13:59:32 -0400

    building icons on map

--- src/components/Map.js
+++ src/components/Map.js
@@ -3,12 +3,59 @@ import Map from 'ol/Map';
 import OSM from 'ol/source/OSM';
 import TileLayer from 'ol/layer/Tile';
 import View from 'ol/View';
-import { fromLonLat } from 'ol/proj'
+import { fromLonLat, transform } from 'ol/proj'
+import VectorSource from 'ol/source/Vector';
+import { Vector as VectorLayer } from 'ol/layer';
+import Point from 'ol/geom/Point';
+import Feature from 'ol/Feature';
+import {Fill, RegularShape, Stroke, Style} from 'ol/style';
 
 function UsfMap() {
 
+  let map
+  let vectorLayer
+  let polyCoords
+  const buildings = [
+    {
+      name: 'LIB',
+      coords: [-82.412220, 28.059588]
+    }, 
+    {
+      name: 'BSN',
+      coords: [-82.409976, 28.058358]
+    }, 
+    {
+      name: 'CPR',
+      coords: [-82.410845, 28.059827]
+    },
+    {
+      name: 'HMS',
+      coords: [-82.409271, 28.060856]
+    }
+  ]
+  const stroke = new Stroke({ color: 'black', width: 2 });
+  const fill = new Fill({ color: 'red' });
+
+  const styles = {
+    'square': new Style({
+      image: new RegularShape({
+        fill,
+        stroke,
+        points: 4,
+        radius: 10,
+        angle: Math.PI / 4,
+      }),
+    })
+  }
+
+  const styleKeys = [
+    'square',
+  ];
+
+  const features = new Array(buildings.length);
+
   useEffect(() => {
-    new Map({
+    map = new Map({
       layers: [
         new TileLayer({
           source: new OSM(),
@@ -20,14 +67,38 @@ function UsfMap() {
         zoom: 16,
       }),
     });
+
+    for(var i = 0; i < buildings.length; i++) {
+      features[i] = new Feature(new Point(fromLonLat(buildings[i].coords)));
+      features[i].setStyle(
+        styles[styleKeys[Math.floor(Math.random() * styleKeys.length)]]
+      );
+    }
+
+    const source = new VectorSource({
+      features,
+    });
+    
+    vectorLayer = new VectorLayer({ source });
+
+    map.addLayer(vectorLayer)
+
+    map.on('click', e => {
+      map.forEachFeatureAtPixel(e.pixel, function (feature, layer) {
+          polyCoords = [
+            feature
+            // Store coords of where the click was to do polyline
+          ]
+      })
+    })
     
   }, []) 
 
-    return (
-        <div className="map-container">
-            <div className='map' id='map'></div>
-        </div>
-    );
+  return (
+    <div className="map-container">
+      <div className='map' id='map'></div>
+    </div>
+  );
 }
 
 export default UsfMap;
