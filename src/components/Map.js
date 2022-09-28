b8d307a8e48b1ede509b0420ef42c09e094ef28b
Adrian <grillo.adrian13@gmail.com>
2022-09-28 13:51:24 -0400

    merge commit

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react'
 import Map from 'ol/Map';
 import OSM from 'ol/source/OSM';
 import TileLayer from 'ol/layer/Tile';
@@ -6,18 +7,21 @@ import { fromLonLat } from 'ol/proj'
 
 function UsfMap() {
 
+  useEffect(() => {
     new Map({
-        layers: [
-          new TileLayer({
-            source: new OSM(),
-          }),
-        ],
-        target: 'map',
-        view: new View({
-          center: fromLonLat([-82.414863, 28.061899]),
-          zoom: 16,
+      layers: [
+        new TileLayer({
+          source: new OSM(),
         }),
-      });
+      ],
+      target: 'map',
+      view: new View({
+        center: fromLonLat([-82.414863, 28.061899]),
+        zoom: 16,
+      }),
+    });
+    
+  }, []) 
 
     return (
         <div className="map-container">
