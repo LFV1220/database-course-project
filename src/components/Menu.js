167275052269e6537126c179030c15b7e1a69b67
Luis F. Vega <vegaluis1220@gmail.com>
2022-09-28 16:25:22 -0400

    working on menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,32 +1,53 @@
 import Dropdown from 'react-bootstrap/Dropdown';
+import Form from 'react-bootstrap/Form';
 
 function Menu() {
     return (
         <div className="menu-container">
-            <h2 style={{'fontWeight': '300'}}>Classes</h2>
+            <h2 style={{ 'fontWeight': '300' }}>Class Schedule</h2>
 
-            <Dropdown className='dropdown'>
+            {["Day", "Day", "Day", "Day", "Day"].map(
+                (d) => (
+                    <div>
+                        <Dropdown className="dropdown">
+                            <Dropdown.Toggle id="dropdown-button-dark-example1" variant="secondary">
+                                {d}
+                            </Dropdown.Toggle>
+
+                            <Dropdown.Menu variant="dark">
+                                <Dropdown.Item href="#/action-1">Monday</Dropdown.Item>
+                                <Dropdown.Item href="#/action-2">Tuesday</Dropdown.Item>
+                                <Dropdown.Item href="#/action-3">Wednesday</Dropdown.Item>
+                                <Dropdown.Item href="#/action-4">Thursday</Dropdown.Item>
+                                <Dropdown.Item href="#/action-5">Friday</Dropdown.Item>
+                                <Dropdown.Item href="#/action-6">Saturday</Dropdown.Item>
+                                <Dropdown.Item href="#/action-7">Sunday</Dropdown.Item>
+                            </Dropdown.Menu>
+                        </Dropdown>
+
+                        <Form className="form">
+                            <Form.Group className="mb-3">
+                                <Form.Control type="building" placeholder="Enter building..." />
+                            </Form.Group>
+                        </Form>
+                    </div>
+
+                ),
+            )}
+
+            {/* <Dropdown className='dropdown'>
                 <Dropdown.Toggle id="dropdown-button-dark-example1" variant="secondary">
-                    Dropdown Button
+                    Day
                 </Dropdown.Toggle>
 
                 <Dropdown.Menu variant="dark">
-                <Dropdown.Item href="#/action-1" active>
-                    Action
-                </Dropdown.Item>
-                <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-                <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-                <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-                <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-                <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-                <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-                <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-                <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-                <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-                <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-                <Dropdown.Divider />
+                    <Dropdown.Item href="#/action-1" active>
+                        Action
+                    </Dropdown.Item>
+                    <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+                    <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
                 </Dropdown.Menu>
-            </Dropdown>
+            </Dropdown> */}
         </div>
     );
 }
