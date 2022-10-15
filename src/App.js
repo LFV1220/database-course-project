4dbd6758925ab0307744a2a5bc1d0f15412d6238
Luis F. Vega <vegaluis1220@gmail.com>
2022-10-14 22:02:51 -0400

    need to fix input into array

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import Header from "./components/Header";
 import UsfMap from "./components/Map";
 import Menu from "./components/Menu";
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,6 +1,10 @@
 import React, { useState } from 'react';
+import { ImageSourceEventType } from 'ol/source/Image';
+//import { fromPascal } from 'postgres';
+import { FormControl } from 'react-bootstrap';
 import Dropdown from 'react-bootstrap/Dropdown';
 import Form from 'react-bootstrap/Form';
+import { getElementError } from '@testing-library/react';
 
 function Menu() {
     // dropdown value input
@@ -10,30 +14,46 @@ function Menu() {
         setValue(e);
     }
 
+    //TODO: work on getting input on array correctly
+    let tempSaved = new Array();
+    function handleInputChange(e) {
+        // if (e.target.value.length === 3) {
+
+        // }
+
+        form.map(evt => {
+
+        }
+
+        );
+
+
+    }
+
     // form array
     const [form, setForm] = useState([
         (<div>
             <Form.Text>Building 1:</Form.Text>
             <Form.Group className="mb-3">
-                <Form.Control className="input-field" type="text" placeholder="Enter building code..." maxLength="3" />
+                <Form.Control className="input-field" key={0} onChange={handleInputChange} type="text" placeholder="Enter building code..." maxLength="3" />
             </Form.Group>
         </div>),
         (<div>
             <Form.Text>Building 2:</Form.Text>
             <Form.Group className="mb-3">
-                <Form.Control className="input-field" type="text" placeholder="Enter building code..." maxLength="3" />
+                <Form.Control className="input-field" key={1} onChange={handleInputChange} type="text" placeholder="Enter building code..." maxLength="3" />
             </Form.Group>
         </div>),
         (<div>
             <Form.Text>Building 3:</Form.Text>
             <Form.Group className="mb-3">
-                <Form.Control className="input-field" type="text" placeholder="Enter building code..." maxLength="3" />
+                <Form.Control className="input-field" key={2} type="text" placeholder="Enter building code..." maxLength="3" />
             </Form.Group>
         </div>),
         (<div>
             <Form.Text>Building 4:</Form.Text>
             <Form.Group className="mb-3">
-                <Form.Control className="input-field" type="text" placeholder="Enter building code..." maxLength="3" />
+                <Form.Control className="input-field" key={3} type="text" placeholder="Enter building code..." maxLength="3" />
             </Form.Group>
         </div>)]);
 
@@ -43,7 +63,7 @@ function Menu() {
             <div>
                 <Form.Text>Building {form.length + 1}:</Form.Text>
                 <Form.Group className="mb-3">
-                    <Form.Control className="input-field" type="text" placeholder="Enter building code..." maxLength="3" />
+                    <Form.Control className="input-field" key={form.length} type="text" placeholder="Enter building code..." maxLength="3" />
                 </Form.Group>
             </div>
         );
@@ -58,6 +78,42 @@ function Menu() {
         }
         setForm(newForms);
     }
+
+    // saved buildings
+    const savedBuildings = {
+        monday: [],
+        tuesday: [],
+        wednesday: [],
+        thursday: [],
+        friday: [],
+        saturday: [],
+        sunday: []
+    };
+
+    // TODO: function to check value (with switch case) then setSaved to certain day to add form data to the certain array
+    // TODO: also fix input, where building number corresponds to tempSaved index
+    // const handleSave = () => {
+    //     switch (value) {
+    //         case "Monday":
+    //             // something like savedBuildings.{value} = tempSaved in here
+    //             return
+    //         case "Tuesday":
+    //             return
+    //         case "Wednesday":
+    //             return
+    //         case "Thursday":
+    //             return
+    //         case "Friday":
+    //             return
+    //         case "Saturday":
+    //             return
+    //         case "Sunday":
+    //             return
+    //         default:
+    //             return null;
+    //     }
+
+    // }
 
 
     return (
