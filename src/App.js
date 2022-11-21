979daeaeae7e014bb53bc01686664fc2c4756941
Adrian <grillo.adrian13@gmail.com>
2022-11-20 21:10:18 -0500

    added login page

--- src/App.js
+++ src/App.js
@@ -4,19 +4,23 @@ import Header from "./components/Header";
 import UsfMap2 from "./components/Map2";
 // import Menu from "./components/Menu";
 import Menu2 from "./components/Menu2";
+import Login from "./components/Login"
 
 function App() {
   const [buildingsList, setBuildingsList] = useState([]);
 
   return (
+    // <div className="app">
+    //   <Header />
+    //   <div className='flex main-container'>
+    //     {/* <Menu setBuildingsList={ setBuildingsList } /> */}
+    //     <Menu2 setBuildingsList={ setBuildingsList } />
+    //     {/* <UsfMap buildingsList={ buildingsList } /> */}
+    //     <UsfMap2 buildingsList={ buildingsList } />
+    //   </div>
+    // </div>
     <div className="app">
-      <Header />
-      <div className='flex main-container'>
-        {/* <Menu setBuildingsList={ setBuildingsList } /> */}
-        <Menu2 setBuildingsList={ setBuildingsList } />
-        {/* <UsfMap buildingsList={ buildingsList } /> */}
-        <UsfMap2 buildingsList={ buildingsList } />
-      </div>
+      <Login />
     </div>
   );
 }
---
+++ src/components/Login.js
@@ -0,0 +1,34 @@
+import { useState } from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Form';
+
+const Login = () => {
+
+
+    return (
+        <div className='flex login-container-wrapper'>
+            <div className='flex login-container'>
+                <h1>Login</h1>
+                <Form>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" />
+                        <Form.Text className="text-muted">
+                        Luis Vega, Joshua Gourlay, and Adrian Grillo
+                        </Form.Text>
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password" />
+                    </Form.Group>
+                    <Button variant="primary" type="submit">
+                        Submit
+                    </Button>
+                </Form>
+            </div>
+        </div>
+    )
+}
+
+export default Login;
