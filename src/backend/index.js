fa78462eb5b6f89007c961e6bd37df0f9b9dfcc8
Luis F. Vega <vegaluis1220@gmail.com>
2022-11-28 20:43:27 -0500

    database functions outlines

--- src/backend/index.js
+++ src/backend/index.js
@@ -16,16 +16,51 @@ const db = knex({
     },
 });
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-})
-
-app.listen(port, () => {
-  console.log(`App running on port ${port}.`);
-})
-
 // Database functions
 // GET: Fetches things from database
+
 // POST: Create and add them to the database
+// Create a new user 
+// insert into users values (email, password, current_date, graduation_date)
+// something like this (not completely correct yet)
+app.post('/', (req, res) => {
+  const {email, password, current_date, graduation_date} = req.body;
+  db('users')
+    .insert({
+      Email: email,
+      Password: password,
+      Current_Date: current_date,
+      Graduation_Date: graduation_date
+    })
+    .then(() => {
+      console.log("user added");
+      return res.body({ msg: "user added"});
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+// Create a new building
+// insert into building values(buildingprefix, longitude, latitude)
+// not completely correct yet
+app.post('/', (req, res) => {
+  const {buildingprefix, longitude, latitude} = req.body;
+  db('building')
+    .insert({
+      BuildingPrefix: buildingprefix,
+      Longitude: longitude,
+      Latitude: latitude
+    })
+    .then(() => {
+      console.log("building added");
+      return res.body({ msg: "building added"});
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
 // DELETE: Removes them from the database
+
 // PUT: Updates things in the database
