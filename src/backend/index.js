12a2baa38434a1d5fb5ce9992c938d756fd61124
Luis F. Vega <vegaluis1220@gmail.com>
2022-11-28 22:41:12 -0500

    setting up postgres

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,65 +1,48 @@
 const express = require('express');
-const cors = require('cors');
-const knex = require('knex');
 const app = express();
-app.use(cors());
-require('dotenv').config();
+const { Pool } = require("pg");
 const port = 3001;
 
-const db = knex({
-    client: 'pg',
-    connection: {
-        host: process.env.DATABASE_HOST,
-        user: process.env.DATABASE_USERNAME,
-        password: process.env.DATABASE_PASSWORD,
-        database: process.env.DATABASE,
-    },
+app.use(express.json())
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
+  next();
+});
+
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'db-app',
+  password: 'root',
+  port: 5432
+});
+
+pool.connect();
+
+// query example
+pool.query(`Select * from users`, (err, res) => {
+  if (!err) {
+    console.log(res.rows);
+  } else {
+    console.log(err.message);
+  }
+  pool.end;
 });
 
 // Database functions
 // GET: Fetches things from database
 
 // POST: Create and add them to the database
-// Create a new user 
+
+// Create a new user
 // insert into users values (email, password, current_date, graduation_date)
-// something like this (not completely correct yet)
-app.post('/', (req, res) => {
-  const {email, password, current_date, graduation_date} = req.body;
-  db('users')
-    .insert({
-      Email: email,
-      Password: password,
-      Current_Date: current_date,
-      Graduation_Date: graduation_date
-    })
-    .then(() => {
-      console.log("user added");
-      return res.body({ msg: "user added"});
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
+
 
 // Create a new building
 // insert into building values(buildingprefix, longitude, latitude)
-// not completely correct yet
-app.post('/', (req, res) => {
-  const {buildingprefix, longitude, latitude} = req.body;
-  db('building')
-    .insert({
-      BuildingPrefix: buildingprefix,
-      Longitude: longitude,
-      Latitude: latitude
-    })
-    .then(() => {
-      console.log("building added");
-      return res.body({ msg: "building added"});
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
+
 
 // DELETE: Removes them from the database
 
