2511d4b422301492a03c2cc8c43f76fd887de9f7
Luis F. Vega <vegaluis1220@gmail.com>
2022-12-01 14:28:08 -0500

    setting up feedback button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,18 @@
+import React, { useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import { Link } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Modal from 'react-bootstrap/Modal'
 
 function Header({ isSignedIn, setSignedIn }) {
 
     const signOut = () => setSignedIn(false);
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
 
     return (
         <>
@@ -19,9 +27,52 @@ function Header({ isSignedIn, setSignedIn }) {
                             className="d-inline-block align-top"
                         />
                     </Navbar.Brand>
-                    { isSignedIn
-                    ? <Link to='/Login'><button className='signout-btn' onClick={() => signOut()}>Sign Out</button></Link>
-                    : <Link to='/Login'><button className='signout-btn'>Sign In</button></Link> }
+                    {isSignedIn
+                        ? <Link to='/Login'><button className='signout-btn' onClick={() => signOut()}>Sign Out</button></Link>
+                        : <Link to='/Login'><button className='signout-btn'>Sign In</button></Link>}
+
+                    {isSignedIn
+                        ? <div>
+                            <Button variant="primary" onClick={handleShow}>
+                                Test heading
+                            </Button>
+                            <Modal show={show} onHide={handleClose}>
+                                <Modal.Header closeButton>
+                                    <Modal.Title>Modal heading</Modal.Title>
+                                </Modal.Header>
+                                <Modal.Body>
+                                    <Form>
+                                        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                                            <Form.Label>Email address</Form.Label>
+                                            <Form.Control
+                                                type="email"
+                                                placeholder="name@example.com"
+                                                autoFocus
+                                            />
+                                        </Form.Group>
+                                        <Form.Group
+                                            className="mb-3"
+                                            controlId="exampleForm.ControlTextarea1"
+                                        >
+                                            <Form.Label>Example textarea</Form.Label>
+                                            <Form.Control as="textarea" rows={3} />
+                                        </Form.Group>
+                                    </Form>
+                                </Modal.Body>
+                                <Modal.Footer>
+                                    <Button variant="secondary" onClick={handleClose}>
+                                        Close
+                                    </Button>
+                                    <Button variant="primary" onClick={handleClose}>
+                                        Save Changes
+                                    </Button>
+                                </Modal.Footer>
+                            </Modal>
+                        </div>
+
+
+
+                        : null}
                 </Container>
             </Navbar>
         </>
