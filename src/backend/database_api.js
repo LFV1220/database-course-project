f8c8bfe46f8fd603bfd9c34f49bdb115db7984da
Luis F. Vega <vegaluis1220@gmail.com>
2022-12-01 12:44:23 -0500

    updated backend with stuff from master

---
+++ src/backend/database_api.js
@@ -0,0 +1,63 @@
+const express = require('express')
+const app = express()
+const port = 3001
+
+const queries = require('./queries')
+
+app.use(express.json())
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
+    next();
+});
+
+
+app.get('/', (req, res) => {
+    queries.getUserRoutes()
+        .then(response => {
+            res.status(200).send(response);
+        })
+        .catch(error => {
+            res.status(500).send(error);
+        })
+})
+app.post('/user', (req, res) => {
+    queries.insertUser(req.body)
+        .then(response => {
+            res.status(200).send(response);
+        })
+        .catch(error => {
+            res.status(500).send(error);
+        })
+})
+app.post('/classes', (req, res) => {
+    queries.insertClasses(req.body)
+        .then(response => {
+            res.status(200).send(response);
+        })
+        .catch(error => {
+            res.status(500).send(error);
+        })
+})
+app.post('/building', (req, res) => {
+    queries.insertBuilding(req.body)
+        .then(response => {
+            res.status(200).send(response);
+        })
+        .catch(error => {
+            res.status(500).send(error);
+        })
+})
+app.delete('/classes/:email', (req, res) => {
+    queries.deleteClasses(req.params.email)
+        .then(response => {
+            res.status(200).send(response);
+        })
+        .catch(error => {
+            res.status(500).send(error);
+        })
+})
+app.listen(port, () => {
+    console.log(`App running on port ${port}.`)
+})
--- src/index.js
+++ src/index.js
@@ -6,10 +6,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <div>
-      <App />
-    </div>
-  </React.StrictMode>
+  <div>
+    <App />
+  </div>
 );
 
